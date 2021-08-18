d84c9fb7f483643c6edd2775f6624e3a430830c2
Sidi BA <sasuke_uchiwa@localhost.localdomain>
2021-08-18 06:07:11 -0400

    [UPLOAD] mysql tables

--- epytodo.sql
+++ epytodo.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS user(
     password PASSWORD(CHAR(50)),
     name VARCHAR(50) NOT NULL,
     firstname VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP,
+    created_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS todo(
@@ -16,6 +16,6 @@ CREATE TABLE IF NOT EXISTS todo(
     created_at TIMESTAMP,
     due_time VARCHAR(50) NOT NULL,
     firstname VARCHAR(50) NOT NULL,
-    status DEFAULT(not started)
+    status VARCHAR(50) DEFAULT(not started),
     user_id VARCHAR(50) NOT NULL
 );

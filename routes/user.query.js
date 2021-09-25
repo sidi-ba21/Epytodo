94fa0767ed26012c00500c822244e1bc59f71a78
Sidi BA <sasuke_uchiwa@localhost.localdomain>
2021-09-25 14:36:13 +0200

    [DEL] unused file

--- routes/user.query.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-    const user = require("./users.js");
-    // Create a new Customer
-    app.post("/register", user.create);
-    // Retrieve all Customers
-    app.get("/user", user.getAll);
-}
